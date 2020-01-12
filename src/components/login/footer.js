b177bfd0e0b688205a4474dd766324c0923186f8
mdoubobobarry07ca@gmail.com <mdoubobobarry07ca@gmail.com>
2020-01-12 19:34:05 +0100

    navigation

---
+++ src/components/login/footer.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+
+
+import styled from 'styled-components';
+import '../../assets/css/index.css'
+
+
+export default class Footer extends React.Component{
+    render(){
+        return(
+
+
+
+   /*    <NavWrapper className="navbar navbar-expand-sm  navbar-dark px-sm-5 mb-0" >
+
+
+<ul class="nav justify-content-center ml-5">
+  <li class="nav-item ml-5">
+    <h4>Acheter </h4>
+    <p>Ce site vous permet d'acheter..</p>
+  </li>
+  <li class="nav-item ml-5">
+  <h4>Vendre</h4>
+  <p>Vendre vos produits</p>
+  </li>
+  <li class="nav-item ml-5">
+  <h4>A propos de nous</h4>
+  <p>Bejaia est un site E-Commerce </p>
+  </li>
+  <li class="nav-item ml-5">
+  <h4>Contactez nous</h4>
+  </li>
+</ul> *
+
+
+
+
+      </NavWrapper>
+*/
+ <div className="footer">
+ <h1>Merci d'avoir visité mon site web fictif.</h1>
+</div>
+
+
+
+
+);
+}
+}
+
+
+
+/* const NavWrapper = styled.nav`
+background:#464745;
+.nav-link{
+    color:var(--mainWhitee) !important;
+    font-size:3rem
+    text-transform:capitalize !important;
+}
+
+.nav-link:hover{
+  color:var(--mainYellow) !important;
+  font-size:1.33rem
+  text-transform:capitalize !important;
+}
+
+` */
+
+
+
